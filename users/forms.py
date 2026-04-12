a90aa00d17cfb9dba9f2acdc00898eaaad1cd543
Ikram <ikramxiyasov2@gmail.com>
2026-04-12 20:12:49 +0500

    inital commit

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
+
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2', 'role']
+        fields = ['username', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'user'
+        if commit:
+            user.save()
+        return user
